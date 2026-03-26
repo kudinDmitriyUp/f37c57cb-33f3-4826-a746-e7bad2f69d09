f450b2d1f21780d148bb19344276a94bd0b6cd6c
kudinDmitriyUp <kudin.dmitriy.up@gmail.com>
2026-03-26 12:25:18 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Montserrat } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Diki Flowers Studio | Bespoke Floral Designs',
+  description: 'Expertly crafted floral arrangements by Diki. Delivering fresh, artistic beauty for weddings, events, and everyday life.',
+  openGraph: {
+    "title": "Diki Flowers Studio",
+    "description": "Bespoke floral arrangements for life's special moments.",
+    "siteName": "Diki Flowers"
+  },
+};
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${montserrat.variable} ${inter.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
